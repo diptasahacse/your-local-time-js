c2af5ca7bb3b588b2cff034e14c7f81f1b402ac6
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-28 17:07:11 +0600

    make digital clock

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*
+Project Name : Digital Clock
+Author: Dipta Saha
+Email: diptasaha.lpu.cse@gmail.com
+Date: 28 March 2022
+
+*/
+
+
 let clockArea = document.getElementById('clock-area');
 
 setInterval(()=>{
